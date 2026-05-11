9f52fc064c3543fff7f95967a4678c3a7a7f1c71
zeignsx <zakimoses190@gmail.com>
2026-05-11 20:22:44 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { router } from './router'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { router, queryClient } from './router'
 import './styles.css'
-
-const queryClient = new QueryClient()
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
